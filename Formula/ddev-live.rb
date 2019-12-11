1b57c4cb6ae32cbb6a8cfdeef216bc3bd60220f2
Andrew French <andrew@afren.ch>
2019-12-11 13:43:10 -0700

    Update formatting to pass strict audit

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,17 +1,19 @@
 class DdevLive < Formula
-  desc "ddev-live: the command-line client for the DDEV Live platform"
+  desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
   url "http://downloads.ddev.com/ddev-live-cli/v0.2.5-dev20191119b/darwin/ddev-live.zip"
+  version "0.2.5-dev20191119b"
   sha256 "96e63d86217274a3d83d9a9f2d13aa8933cc3500beca89286c5369070a89499b"
 
   bottle :unneeded
 
   def install
     bin.install "ddev-live"
-  end 
+    bash_completion.install "ddev-live_completion_bash.sh" => "ddev-live"
+    zsh_completion.install "ddev-live_completion_zsh.sh" => "ddev-live"
+  end
 
-  test do 
+  test do
     system "#{bin}/ddev-live", "--version"
   end
-end 
-
+end
