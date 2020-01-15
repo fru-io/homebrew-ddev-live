171016ef1c9eed4664d7d2d089c29f468fc2b7d5
Andrew French <andrew@afren.ch>
2020-01-15 14:10:23 -0300

    Use https asset URL

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,7 +1,7 @@
 class DdevLive < Formula
   desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
-  url "http://downloads.ddev.com/ddev-live-cli/v0.3.0/darwin/ddev-live.zip"
+  url "https://downloads.ddev.com/ddev-live-cli/v0.3.0/darwin/ddev-live.zip"
   version "0.3.0"
   sha256 "bc8318e2b0c0ff21fba913ec3b89a2637476016417be8530c002abf449c5fe0e"
 
