b19f3fe24f3e6f990ddc0efff47e4d52829f8981
Andrew French <andrew@afren.ch>
2020-01-15 14:05:36 -0300

    Bump version to v0.3.0

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,9 +1,9 @@
 class DdevLive < Formula
   desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
-  url "http://downloads.ddev.com/ddev-live-cli/v0.2.5-dev20191211/darwin/ddev-live.zip"
-  version "0.2.5-dev20191211"
-  sha256 "8d90448f0cbf25e9c727bb99c6db955b4213977d139eac47251f777bb926e7a6"
+  url "http://downloads.ddev.com/ddev-live-cli/v0.3.0/darwin/ddev-live.zip"
+  version "0.3.0"
+  sha256 "bc8318e2b0c0ff21fba913ec3b89a2637476016417be8530c002abf449c5fe0e"
 
   bottle :unneeded
 
