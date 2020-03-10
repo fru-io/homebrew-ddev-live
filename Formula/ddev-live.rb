154c796993ce5c55d74ef151a38a33edad16d88d
Jan Wozniak <wozniak.jan@gmail.com>
2020-03-10 12:55:35 +0100

    Bump version to v0.4.2

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,9 +1,9 @@
 class DdevLive < Formula
   desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
-  url "https://downloads.ddev.com/ddev-live-cli/v0.4.0/darwin/ddev-live.zip"
-  version "0.4.0"
-  sha256 "0611ece6606ccb6b08e968b45716117653e02c7b69b71002f3e9ea9f124a7687"
+  url "https://downloads.ddev.com/ddev-live-cli/v0.4.2/darwin/ddev-live.zip"
+  version "0.4.2"
+  sha256 "305c4854d93b534cdf5893cfed41ae3da083851d052c271d788371b35ea3b4d0"
 
   bottle :unneeded
 
