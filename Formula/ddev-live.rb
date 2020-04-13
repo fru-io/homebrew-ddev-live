00305d03d5ed0df48f38efb9367dffd6bf7464d8
Andrew French <andrew@afren.ch>
2020-04-13 15:04:12 -0800

    Bump version to v0.5.1

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,14 +1,19 @@
 class DdevLive < Formula
   desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
-  url "https://downloads.ddev.com/ddev-live-cli/v0.5.0/darwin/ddev-live.zip"
-  version "0.5.0"
-  sha256 "8996152009783edfb3383a7eac5cd8e585a3106b043a8f529554412f764d3b01"
+  url "https://downloads.ddev.com/ddev-live-cli/v0.5.1/brew/ddev-live.zip"
+  version "0.5.1"
+  sha256 "ie534933ca2debe03ec43e15a2e246af0b600836bcb7731b6f1fa6bcd9c4a60ab"
 
   bottle :unneeded
 
   def install
-    bin.install "ddev-live"
+    if OS.mac?
+      bin.install "darwin/ddev-live"
+    else
+      bin.install "linux/ddev-live"
+    end
+
     bash_completion.install "ddev-live_completion_bash.sh" => "ddev-live"
     zsh_completion.install "ddev-live_completion_zsh.sh" => "ddev-live"
   end
