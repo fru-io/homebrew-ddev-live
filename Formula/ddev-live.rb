ae48e7cd4b5d71b8cbc965e88b989f488678cf6e
Derek Lemon <derlemon@gmail.com>
2021-02-02 15:58:38 -0700

    Bumping version to v0.9.2

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,9 +1,9 @@
 class DdevLive < Formula
   desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
-  url "https://downloads.ddev.com/ddev-live-cli/v0.7.0/brew/ddev-live.zip"
-  version "0.7.0"
-  sha256 "df25d10ca7f5f9d9cdff3ab489a24c8b27b3493dc471ae7742e8cba577dc698a"
+  url "https://downloads.ddev.com/ddev-live-cli/v0.9.2/brew/ddev-live.zip"
+  version "0.9.2"
+  sha256 "29ed47eff12c29fa61f7579473f7d7a99a9056ba1546d1cf9c2e24d318b49a5c"
 
   bottle :unneeded
 
